ffbed29c790f608b9e88a25400be13a5a1bbc488
Emmanuel Pelletier <emmanuel.pelletier@ecolutis.com>
2015-03-01 12:22:32 +0100

    reworking config schema

--- src/Config.php
+++ src/Config.php
@@ -2,11 +2,14 @@
 
 namespace BigHubBrother;
 
+use Exception;
+
 class Config
 {
     public function __construct($filepath)
     {
         $this->config = json_decode(file_get_contents($filepath), true);
+        $this->_prepare();
         $this->_parse();
     }
 
@@ -15,19 +18,47 @@ class Config
         return $this->config;
     }
 
-    public function isRepoWatchedByUser($repo, $user)
+    public function getUsers()
     {
-        $repos = $this->config['users'][$user]['repos'];
-        return !empty($repos[$repo]);
+        return $this->config['users'];
     }
 
-    // yeah.
-    public function isCommitterWatchedByUserAndRepo($committer, $user, $repo)
+    public function getUser($user)
+    {
+        return $this->config['users'][$user];
+    }
+
+    public function getRepo($repo, $user)
+    {
+        $userConf = $this->getUser($user);
+        $default = $userConf['default'];
+        $blacklist = $userConf['exclude'];
+        $whitelist = $userConf['only'];
+        $specifics = $userConf['specifics'];
+
+        if (!empty($specifics[$repo]))
+            return $specifics[$repo];
+
+        if (!empty($default) && (
+            (!empty($blacklist) && !in_array($repo, $blacklist)) ||
+            (!empty($whitelist) && in_array($repo, $whitelist))
+        ))
+            return $default;
+
+        return null;
+    }
+
+    public function isRepoWatchedByUser($repo, $user)
+    {
+        return !!$this->getRepo($repo, $user);
+    }
+
+    public function isCommitterWatchedByUserInRepo($committer, $user, $repo)
     {
         if ($committer === $user)
             return false;
 
-        $repo = $this->config['users'][$user]['repos'][$repo] ?: null;
+        $repo = $this->getRepo($repo, $user);
         if (!$repo)
             return false;
 
@@ -40,31 +71,73 @@ class Config
         return true;
     }
 
+    public function areFilesWatchedByUserInRepo($files, $user, $repo)
+    {
+        $repo = $this->getRepo($repo, $user);
+        if (!$repo)
+            return false;
+
+        $watched = false;
+        $watchedFiles = $repo['file_patterns'];
+        foreach ($watchedFiles as $pattern) {
+            foreach ($files as $file) {
+                if (fnmatch($pattern, $file)) {
+                    $watched = true;
+                }
+            }
+        }
+        return $watched;
+    }
+
+    protected function _prepare()
+    {
+        if (empty($this->config['users']))
+            $this->config['users'] = [];
+
+        foreach ($this->config['users'] as $name => &$info) {
+
+            if (!empty($info['exclude']) && !empty($info['only'])) {
+                throw new Exception("$name: can't have exclude and only at same time", 1);
+            }
+
+            foreach (['default', 'specifics', 'exclude', 'only'] as $key) {
+                if (empty($info[$key]))
+                    $info[$key] = [];
+            }
+        }
+    }
+
     protected function _parse()
     {
         $conf = $this->config;
-        foreach ($conf['users'] as $username => $info) {
-            $conf['users'][$username]['repos'] = $this->_mergeUserWildcardRepos($info['repos']);
+        foreach ($conf['users'] as &$info) {
+            $info['specifics'] = $this->_mergeUserDefaultConfs($info);
         }
+
         $this->config = $conf;
     }
 
-    protected function _mergeUserWildcardRepos($repos)
+    protected function _mergeUserDefaultConfs($userConf)
     {
-        $newRepos = [];
+        if (empty($userConf['default']))
+            return $userConf['specifics'];
 
-        $wildcard = !empty($repos["*"]) ? $repos["*"] : [];
-        $noMergeRepos = !empty($wildcard['except_repos']) ? $wildcard['except_repos'] : [];
-        unset($wildcard['except_repos']);
+        $default = $userConf['default'];
+        $specifics = $userConf['specifics'];
+        $except = $userConf['exclude'];
+        $only = $userConf['only'];
 
-        foreach ($repos as $name => $info) {
-            if ($name === "*")
+        foreach ($specifics as $name => $info) {
+            if ( !empty($except) && in_array($name, $except) )
                 continue;
-            $newRepos[$name] = (!in_array($name, $noMergeRepos)) ?
-                array_merge_recursive($wildcard, $info) :
-                $info;
+
+            elseif ( !empty($only) && !in_array($name, $only) )
+                continue;
+
+            else
+                $specifics[$name] = Utility::merge($default, $info);
         }
 
-        return $newRepos;
+        return $specifics;
     }
 }
---
+++ src/Utility.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BigHubBrother;
+
+class Utility
+{
+    /** https://github.com/cakephp/cakephp/blob/master/lib/Cake/Utility/Hash.php
+     *
+     * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+     * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+     *
+     * Licensed under The MIT License
+     *
+     * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+     * @link          http://cakephp.org CakePHP(tm) Project
+     * @package       Cake.Utility
+     * @since         CakePHP(tm) v 2.2.0
+     * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+     **/
+    public static function merge(array $data, $merge) {
+        $args = array_slice(func_get_args(), 1);
+        $return = $data;
+        foreach ($args as &$curArg) {
+            $stack[] = array((array)$curArg, &$return);
+        }
+        unset($curArg);
+        while (!empty($stack)) {
+            foreach ($stack as $curKey => &$curMerge) {
+                foreach ($curMerge[0] as $key => &$val) {
+                    if (!empty($curMerge[1][$key]) && (array)$curMerge[1][$key] === $curMerge[1][$key] && (array)$val === $val) {
+                        $stack[] = array(&$val, &$curMerge[1][$key]);
+                    } elseif ((int)$key === $key && isset($curMerge[1][$key])) {
+                        $curMerge[1][] = $val;
+                    } else {
+                        $curMerge[1][$key] = $val;
+                    }
+                }
+                unset($stack[$curKey]);
+            }
+            unset($curMerge);
+        }
+        return $return;
+    }
+}
